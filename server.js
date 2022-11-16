922e7e6ac270d5b5190bcfc529907de6ae9d6b7e
fiau <didan.mobe@gmail.com>
2022-11-16 12:07:19 +0700

    add code

--- server.js
+++ server.js
@@ -27,9 +27,25 @@ app.use(
     extended: true,
   })
 );
+
+const allowedDomains = [
+  process.env.CLIENT_URL_VERCEL,
+  process.env.ADMIN_URL_VERCEL,
+  process.env.CLIENT_URL,
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL_VERCEL,
+    origin: function (origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
+
+      if (allowedDomains.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
