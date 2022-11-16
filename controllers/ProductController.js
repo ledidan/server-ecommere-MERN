cc4db0f9f6ef708ab99be140b2aa4ba08ca3941f
ledidan <didan.mobe@gmail.com>
2022-11-16 20:36:51 +0700

    add codE

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -66,8 +66,11 @@ const deleteProductByAdmin = asyncHandler(async (req, res) => {
 // ?@access  Private
 const createProductByAdmin = asyncHandler(async (req, res) => {
   // Declare Object need to be created
-  const categoryId = await Category.findById(req.body.category);
+  const categoryFound = await Category.findById(req.body.category);
 
+  if (categoryFound) {
+    return res.status(400).json({ message: "Category Not Found" });
+  }
   const { name, price, description, image, countInStock, category } = req.body;
 
   // ? Check Exist Product
@@ -81,12 +84,12 @@ const createProductByAdmin = asyncHandler(async (req, res) => {
     const product = new Product({
       name,
       price,
-      category,
+      category: categoryFound.name,
       description,
       image,
       countInStock,
     });
-    if (product && categoryId) {
+    if (product) {
       const createProduct = await product.save();
       res.status(201).json(createProduct);
     } else {
@@ -101,9 +104,7 @@ const updateProductByAdmin = asyncHandler(async (req, res) => {
   if (!categoryId) return res.status(400).json({ message: "Invalid Category" });
   const { name, price, description, image, countInStock, category } = req.body;
 
-  const product = await await Product.findById(req.params.id).populate(
-    "category"
-  );
+  const product = await Product.findById(req.params.id).populate("category");
 
   if (product) {
     // * Update by any object
@@ -112,7 +113,7 @@ const updateProductByAdmin = asyncHandler(async (req, res) => {
     product.description = description || product.description;
     product.image = image || product.image;
     product.countInStock = countInStock || product.countInStock;
-    product.category = category || product.category;
+    product.category = category || product.category.name;
     const updateProduct = await product.save();
     res.status(201).json(updateProduct);
   } else {
@@ -206,68 +207,6 @@ const productListCategory = asyncHandler(async (req, res) => {
   });
 });
 
-const productListBySearchAsc = asyncHandler(async (req, res) => {
-  let order = req.query.order ? req.query.order : "asc";
-  let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
-  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
-
-  Product.find()
-    .select("-photo")
-    .populate("category")
-    .sort([[sortBy, order]])
-    .limit(limit)
-    .exec((err, products) => {
-      if (err) {
-        return res.status(400).json({
-          error: "Products not found",
-        });
-      }
-      res.json(products);
-    });
-});
-const productListBySearchDesc = asyncHandler(async (req, res) => {
-  let order = req.body.order ? req.body.order : "desc";
-  let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
-  let limit = req.body.limit ? parseInt(req.body.limit) : 6;
-  let skip = parseInt(req.body.skip);
-  let findArgs = {};
-
-  // console.log(order, sortBy, limit, skip, req.body.filters);
-  // console.log("findArgs", findArgs);
-
-  for (let key in req.body.filters) {
-    if (req.body.filters[key].length > 0) {
-      if (key === "price") {
-        // gte -  greater than price [0-10]
-        // lte - less than
-        findArgs[key] = {
-          $gte: req.body.filters[key][0],
-          $lte: req.body.filters[key][1],
-        };
-      } else {
-        findArgs[key] = req.body.filters[key];
-      }
-    }
-  }
-
-  Product.find(findArgs)
-    .select("-photo")
-    .populate("category")
-    .sort([[sortBy, order]])
-    .skip(skip)
-    .limit(limit)
-    .exec((err, data) => {
-      if (err) {
-        return res.status(400).json({
-          error: "Products not found",
-        });
-      }
-      res.json({
-        size: data.length,
-        data,
-      });
-    });
-});
 module.exports = {
   getAllProduct,
   getSingleProduct,
@@ -278,6 +217,4 @@ module.exports = {
   getAllProductByAdmin,
   listProductRelated,
   productListCategory,
-  productListBySearchDesc,
-  productListBySearchAsc,
 };
--- models/CategoryModel.js
+++ models/CategoryModel.js
@@ -6,14 +6,8 @@ const categorySchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    slug: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     description: {
       type: String,
-      required: true,
     },
     product: {
       type: mongoose.Schema.Types.ObjectId,
